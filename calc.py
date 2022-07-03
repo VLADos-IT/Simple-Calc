d08cbbbaeac46acac698ec482c733063e89f56c8
VLADos <vlcommunicate@gmail.com>
2022-07-04 01:25:40 +0500

    First version
    Born calculator program

---
+++ calc.py
@@ -0,0 +1,20 @@
+a = float(input('Enter first number: '))
+b = float(input('Enter second number: '))
+print('What action you want do with this numbers?')
+action = input()
+
+result = None
+
+if action == '+':
+    result = a + b
+elif action == '-':
+    result = a - b
+elif action == '*':
+    result = a * b
+elif action == '/':
+    result = a / b
+else:
+    print('Not supported action!')
+
+if result is not None:
+    print('Result:', result)
